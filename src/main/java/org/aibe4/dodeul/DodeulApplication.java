e406497a6fa38e40d7adf8ef7990fe634ff07915
JunHyun <junhyun1001@gmail.com>
2026-01-04 22:08:38 +0900

    프로젝트 초기 설정

---
+++ src/main/java/org/aibe4/dodeul/DodeulApplication.java
@@ -0,0 +1,13 @@
+package org.aibe4.dodeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodeulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodeulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aibe4/dodeul/DodeulApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibe4.dodeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodeulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
