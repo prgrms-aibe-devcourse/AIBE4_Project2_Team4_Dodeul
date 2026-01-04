3a63c420cd52b2e5bec56b5d87c561ea484d6451
JunHyun <junhyun1001@gmail.com>
2026-01-04 22:36:50 +0900

    BaseEntity 작성

--- src/main/java/org/aibe4/dodeul/DodeulApplication.java
+++ src/main/java/org/aibe4/dodeul/DodeulApplication.java
@@ -2,12 +2,13 @@ package org.aibe4.dodeul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DodeulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DodeulApplication.class, args);
     }
-
 }
--- src/test/java/org/aibe4/dodeul/DodeulApplicationTests.java
+++ src/test/java/org/aibe4/dodeul/DodeulApplicationTests.java
@@ -7,7 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DodeulApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
