1f9b4ea093f05405c5f04eff56e405c01e693f02
JunHyun <junhyun1001@gmail.com>
2026-01-05 22:45:13 +0900

    feat: 생성자 추가 및 필드 수정
    작성되지 않은 엔티티 때문에 주석 처리 함
    
    #2

--- src/main/java/org/aibe4/dodeul/domain/consultation/model/entity/Message.java
+++ src/main/java/org/aibe4/dodeul/domain/consultation/model/entity/Message.java
@@ -15,19 +15,27 @@ public class Message extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
-    private MessageType type;
+    private MessageType messageType;
 
-    @Column(name = "content")
+    @Column(name = "content", columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "is_read", nullable = false)
     private boolean isRead = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "consultation_room_id")
+    @JoinColumn(name = "consultation_room_id", nullable = false)
     private ConsultationRoom consultationRoom;
 
     //    @ManyToOne(fetch = FetchType.LAZY)
-    //    @JoinColumn(name = "member_id")
-    //    private Member member;
+    //    @JoinColumn(name = "member_id", nullable = false)
+    //    private Member sender;
+
+//    @Builder
+//    public Message(MessageType messageType, String content, ConsultationRoom consultationRoom, Member sender) {
+//        this.messageType = messageType;
+//        this.content = content;
+//        this.consultationRoom = consultationRoom;
+//        this.sender = sender;
+//    }
 }
