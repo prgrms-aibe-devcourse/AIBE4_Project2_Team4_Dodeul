4bf88af83dcd08958d2e16b767db93a3b8714af5
JunHyun <junhyun1001@gmail.com>
2026-01-08 16:34:41 +0900

    refactor: 타입 수정
    #48

--- src/main/java/org/aibe4/dodeul/domain/consultation/model/dto/MessageDto.java
+++ src/main/java/org/aibe4/dodeul/domain/consultation/model/dto/MessageDto.java
@@ -3,21 +3,22 @@ package org.aibe4.dodeul.domain.consultation.model.dto;
 import lombok.Builder;
 import lombok.Getter;
 import org.aibe4.dodeul.domain.consultation.model.entity.Message;
+import org.aibe4.dodeul.domain.consultation.model.enums.MessageType;
 
 @Getter
 @Builder
 public class MessageDto {
     private String content;
-    private String type;
+    private MessageType type;
     private String senderNickname;
     private boolean isMine;
 
     public static MessageDto of(Message message, Long memberId) {
         return MessageDto.builder()
-                .content(message.getContent())
-                .type(message.getMessageType().name())
-                .senderNickname(message.getSender().getNickname())
-                .isMine(message.getSender().getId().equals(memberId))
-                .build();
+            .content(message.getContent())
+            .type(message.getMessageType())
+            .senderNickname(message.getSender().getNickname())
+            .isMine(message.getSender().getId().equals(memberId))
+            .build();
     }
 }
