835fbb0edea8febfb31148472e6fb0e6676c5805
godqhrenf <godqhrenf@gmail.com>
2026-01-08 17:14:01 +0900

    feat: 공통 에러 처리 구현
    #44

---
+++ src/main/java/org/aibe4/dodeul/global/exception/GlobalViewExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.aibe4.dodeul.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aibe4.dodeul.global.response.enums.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice(annotations = Controller.class)
+public class GlobalViewExceptionHandler {
+    /** 나머지 모든 시스템 에러. 500 에러 발생 시 처리 */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e, Model model) {
+        log.error("View System Error", e);
+
+        model.addAttribute("errorMessage", ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+
+        return "error/500";
+    }
+}
