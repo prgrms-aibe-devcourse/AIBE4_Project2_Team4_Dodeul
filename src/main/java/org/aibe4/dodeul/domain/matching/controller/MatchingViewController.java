bca9da2a253213e2eb1e5866115bd703b9ade50e
godqhrenf <godqhrenf@gmail.com>
2026-01-14 12:18:48 +0900

    feat: 멘토 선택 페이지 구현
    #154

--- src/main/java/org/aibe4/dodeul/domain/matching/controller/MatchingViewController.java
+++ src/main/java/org/aibe4/dodeul/domain/matching/controller/MatchingViewController.java
@@ -1,27 +1,54 @@
 package org.aibe4.dodeul.domain.matching.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.aibe4.dodeul.domain.common.service.CommonService;
+import org.aibe4.dodeul.domain.consulting.model.enums.ConsultingTag;
+import org.aibe4.dodeul.domain.recommendation.model.dto.MentorRecommendationResponse;
+import org.aibe4.dodeul.domain.recommendation.service.MentorRecommendationService;
+import org.aibe4.dodeul.global.security.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/matchings")
 public class MatchingViewController {
+
+    private final MentorRecommendationService recommendationService;
+    private final CommonService commonService;
 
     @GetMapping("/new")
     public String showRecommendPage(
         @RequestParam("applicationId") Long applicationId,
         Model model) {
 
+        model.addAttribute("jobTags", commonService.getJobTags().getJobTags());
+        model.addAttribute("skillTags", commonService.getSkillTags().getSkillTags());
+        model.addAttribute("consultingTags", ConsultingTag.values());
         model.addAttribute("applicationId", applicationId);
 
-        // TODO: [godqhrenf] 현재는 임시로 HTML에서 멘토 데이터를 가지고 있음
-        return "matching/recommend-test";
+        return "matching/recommend";
+    }
 
-        // TODO: [godqhrenf] 전체 멘토 조회 API 구현 후 DB에서 읽어온 데이터로 수정 예정
-        // return "matching/recommend-test";
+    @GetMapping("/{applicationId}/recommendation")
+    public String getRecommendFragment(
+        @PathVariable Long applicationId,
+        @AuthenticationPrincipal CustomUserDetails userDetails,
+        Model model) {
+
+        List<MentorRecommendationResponse> recommendedMentors =
+            recommendationService.recommendMentors(userDetails.getMemberId(), applicationId);
+
+        model.addAttribute("mentors", recommendedMentors);
+
+        return "matching/fragments/recommend-list :: recommendList";
     }
 
     @GetMapping("/waiting")
